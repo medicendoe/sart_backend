93befa912bb366f25e9067e5b7196e03239aea0a
Jayki-ZX <danteantu5@gmail.com>
2025-08-17 20:29:05 -0400

    revert configs

--- sart/settings.py
+++ sart/settings.py
@@ -13,8 +13,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 
-from dotenv import load_dotenv
-load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -28,12 +28,12 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG").lower() in ('true', '1')
 
-# ALLOWED_HOSTS = [os.environ.get("DJANGO_ALLOWED_HOSTS").split(",")]
-ALLOWED_HOSTS = [
-  'localhost',
-  '127.0.0.1',
-  '*'
-]
+ALLOWED_HOSTS = [os.environ.get("DJANGO_ALLOWED_HOSTS").split(",")]
+# ALLOWED_HOSTS = [
+#   'localhost',
+#   '127.0.0.1',
+#   '*'
+# ]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
@@ -88,23 +88,23 @@ WSGI_APPLICATION = 'sart.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': os.environ.get("DB_NAME"),
-#         'USER': os.environ.get("DB_USER"),
-#         'PASSWORD': os.environ.get("DB_PASSWORD"),
-#         'HOST': os.environ.get("DB_HOST"),
-#         'PORT': '3306',
-#     }
-# }
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "mydatabase",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': '3306',
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": "mydatabase",
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
